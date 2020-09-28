2103d7e54e82a4addde8ee2869352dde85ef331f
yokutoN <strex.5575@gmail.com>
2020-09-28 22:21:08 +0900

    add username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true, length: {minimum: 1, maximum: 15}
+
 end
